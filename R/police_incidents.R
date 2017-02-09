893df2e78e86543520ee64dc06dceec845787d9b
Jacob Kaplan <jacobkap@sas.upenn.edu>
2017-02-09 09:09:11 -0500

    added month_year column

--- R/police_incidents.R
+++ R/police_incidents.R
@@ -122,11 +122,14 @@ coordinate_splitter <- function(dataset, column_name) {
 #' 1 + 3
 date_column_maker <- function(dataset) {
 
-  dataset[, c("year", "month", "day", "weekday") :=
+  dataset[, c("year", "month", "day", "weekday", "month_year") :=
             list(lubridate::year(date_column),
                  lubridate::month(date_column, label = TRUE, abbr = FALSE),
                  lubridate::day(date_column),
-                 lubridate::wday(date_column, label = TRUE, abbr = FALSE))]
+                 lubridate::wday(date_column, label = TRUE, abbr = FALSE),
+                 lubridate::ymd(paste0(lubridate::year(date_column), "-",
+                                       lubridate::month(date_column)),
+                                truncated = TRUE))]
 
 
   return(dataset)
