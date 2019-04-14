a09c237a52780e0d71b13e52ea2a9089811d0a7f
jacobkap <jacobkap@sas.upenn.edu>
2019-04-14 16:25:56 -0400

    add ucr constant reporters function

---
+++ R/ucr_constant_reporters.R
@@ -0,0 +1,31 @@
+ucr_constant_reporter_oris <- function(data,
+                                       start_year,
+                                       end_year,
+                                       minimum_months_reported) {
+
+  if (!minimum_months_reported %in% 0:12) {
+    stop()
+  }
+  constant_oris <- c()
+  data <-
+    data %>%
+    dplyr::filter(year %in% start_year:end_year,
+                  number_of_months_reported >= minimum_months_reported) %>%
+    dplyr::select(ori,
+                  year,
+                  number_of_months_reported)
+
+
+  for (i in start_year:end_year) {
+    temp <-
+      data %>%
+      dplyr::filter(year %in% i)
+    if (i %in% start_year) {
+      constant_oris <- c(constant_oris, unique(temp$ori))
+    } else {
+      constant_oris <- constant_oris[constant_oris %in% unique(temp$ori)]
+    }
+  }
+
+  return(constant_oris)
+}
