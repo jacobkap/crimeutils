c7170fa40ea6526ae4bf2423fd9556236f5a6dd8
jacobkap <jacobkap@sas.upenn.edu>
2019-10-22 19:40:04 -0400

    Adds some cleaning utils

--- R/cleaning_utils.R
+++ R/cleaning_utils.R
@@ -1,30 +1,47 @@
-#
-# load("C:/Users/user/Dropbox/Penn/PhD/Research/cops_and_jails/data/clean/final_data.rda")
-# test <- indicate_outliers(data             = final_data,
-#                           select_numeric_variables = c("fire_payroll",
-#                                                        "cops_payroll"),
-#                           group_variable   = "state_county_name",
-#                           zero_is_outlier  = TRUE)
-#
-# #
-# time_series_data_graph(data             = final_data,
-#                        numeric_variable = "fire_payroll",
-#                        time_variable    = "year",
-#                        group_variable   = "state_county_name",
-#                        file_name        = "firefighter_pay_graph.pdf")
-# #
-# scatterplot_data_graph(final_data,
-#                        numeric_variable1 = "firefighter_full_time_payroll",
-#                        numeric_variable2 = "police_full_time_payroll",
-#                        group_variable    = "state_county_name",
-#                        file_name         = "police_firefighter_scatterplot.pdf")
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+#' Prepare .tex output from kableExtra for Overleaf
+#'
+#'# When kableExtra makes .tex files it adds lines to create a full .tex
+# document (e.g. \start{document}, and adds packages). This removes all that
+# stuff to it can be added to Overleaf.
+#'
+#' @param files
+#' A string or vector of strings naming the .tex files to use.
+#' @param all_tex
+#' A binary variable (default is FALSE) where if TRUE applies code to every .tex
+#' file in working directory. Overrides any value in `files`.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clean_tex_files <- function(files = "", all_tex = FALSE) {
+  if (all_tex) {
+    files = list.files(pattern = ".tex")
+  }
+  for (.file in files) {
+    .tex <- read_lines(.file)
+    .tex <- .tex[grep("begin\\{table\\}", .tex):grep("end\\{table\\}", .tex)]
+    write_lines(.tex, path = .file)
+  }
+}
+
+#' Capitalizes the first letter of every word
+#'
+#' @param word
+#' A string with words you want capitalized
+#'
+#' @return
+#' The origin string with the first letter of each word capitalized
+#' @export
+#'
+#' @examples
+simple_cap <- function(word) {
+  word <- tolower(word)
+  word <- gsub("_", " ", word)
+  word <- strsplit(word, " ")[[1]]
+  word <- paste(toupper(substring(word, 1,1)),
+                substring(word, 2),
+                sep = "",
+                collapse = " ")
+  return(word)
+}
